eae987a00bd584d0de906d04cdead83bccc69084
Quinn <quinn@sportshubtech.com>
2018-08-04 08:55:43 -0400

    Installed material ui library

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { DefaultTheme, FABGroup, Provider as PaperProvider } from 'react-native-paper';
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    // primary: 'tomato',
+    // accent: 'yellow',
+  },
+};
 
 export default class App extends React.Component {
+  state = {
+    open: false,
+  };
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <PaperProvider theme={theme}>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app (FAB!)!</Text>
+          <FABGroup
+            open={this.state.open}
+            icon={this.state.open ? 'today' : 'add'}
+            actions={[
+              { icon: 'add', onPress: () => {} },
+              { icon: 'star', label: 'Star', onPress: () => {} },
+              { icon: 'email', label: 'Email', onPress: () => {} },
+              { icon: 'notifications', label: 'Remind', onPress: () => {} },
+            ]}
+            onStateChange={({ open }) => this.setState({ open })}
+            onPress={() => {
+              if (this.state.open) {
+                // do something if the speed dial is open
+              }
+            }}
+          />
+        </View>
+      </PaperProvider>
     );
   }
 }
